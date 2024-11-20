48ecbe0627346dbf378b03952182042ba61f86af
xZeuDen <162120589+xZeuDen@users.noreply.github.com>
2024-11-20 15:44:10 +0000

    full folder

---
+++ Challange 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timer = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timer <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timer = 1;
+        }
+        timer -= Time.deltaTime;
+    }
+}
